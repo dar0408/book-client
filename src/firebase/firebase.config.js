dd4a8a49262788499b2e5ed13be05666910a22c5
dar0408 <dsalgotra2021@gmail.com>
2024-09-05 09:55:11 +0530

    firebase change

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,3 +1,23 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBrESmF85Oo_XpYFf75OTzFipm91uhBclk",
+//   authDomain: "book-shelf-7b33e.firebaseapp.com",
+//   projectId: "book-shelf-7b33e",
+//   storageBucket: "book-shelf-7b33e.appspot.com",
+//   messagingSenderId: "77306232175",
+//   appId: "1:77306232175:web:81f0af2605b9c828bcfc9a"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,12 +25,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrESmF85Oo_XpYFf75OTzFipm91uhBclk",
-  authDomain: "book-shelf-7b33e.firebaseapp.com",
-  projectId: "book-shelf-7b33e",
-  storageBucket: "book-shelf-7b33e.appspot.com",
-  messagingSenderId: "77306232175",
-  appId: "1:77306232175:web:81f0af2605b9c828bcfc9a"
+  apiKey: "AIzaSyBXZBo_wQxfXSWTJKlxSJ9m3LredsG2mlc",
+  authDomain: "server-c405c.firebaseapp.com",
+  projectId: "server-c405c",
+  storageBucket: "server-c405c.appspot.com",
+  messagingSenderId: "559896217002",
+  appId: "1:559896217002:web:60b0bb0c3a86431c6297d3"
 };
 
 // Initialize Firebase
